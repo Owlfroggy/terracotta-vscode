e44d703cddd2c401d40de63e7c01d24f9fdd3989
MrAwesomeOwl <140653655+MrAwesomeOwl@users.noreply.github.com>
2024-07-21 12:27:36 -0400

    get debug features registered in vscode

---
+++ src/debugger.ts
@@ -0,0 +1,5 @@
+async function main() {
+    console.log("you pushed the debug button!!!!!!!!!!!!!!!!!!!!!!! (wow) (omg) (amazing) (incredible) (99% fail) (cops called) (lost all sanity)")
+}
+
+main()
