897c1b8bf09170ecfa96ef1e1eff5823f807b9b0
MrAwesomeOwl <140653655+MrAwesomeOwl@users.noreply.github.com>
2024-07-22 09:52:50 -0400

    hook up to codeclient

--- src/debugger.ts
+++ src/debugger.ts
@@ -1,5 +1,81 @@
-async function main() {
-    console.log("you pushed the debug button!!!!!!!!!!!!!!!!!!!!!!! (wow) (omg) (amazing) (incredible) (99% fail) (cops called) (lost all sanity)")
+import { DebugProtocol as dap } from "vscode-debugprotocol"
+
+//==========[ util functions ]=========\
+
+function sendEvent(event: string, body: any = null) {
+    let str = JSON.stringify({
+        type: "event",
+        event: event,
+        body: body
+    })
+    
+    process.stdout.write(`Content-Length: ${str.length}\r\n\r\n${str}`)
 }
 
-main()
+function sendResponse(request: dap.Request, body: any, successful: boolean = true) {
+    let str = JSON.stringify({
+        type: "response",
+        request_seq: request.seq,
+        command: request.command,
+        success: successful,
+        body: body
+    })
+    
+    process.stdout.write(`Content-Length: ${str.length}\r\n\r\n${str}`)
+}
+
+//==========[ actual debugger stuff ]=========\
+
+let scopesResolve: ((scopes: string[]) => void) | undefined = undefined
+
+const requestHandlers: {[key: string]: (args: dap.Request) => void} = {
+    "initialize": function(request) {
+        sendResponse(request,{
+            supportsConfigurationDoneRequest: true
+        })
+    },
+    "launch": async function(request) {
+        sendResponse(request,{})
+        
+        if (request.arguments.exportMode == "sendToCodeClient") {
+            let scopes = await new Promise<string[]>(resolve => {
+                sendEvent("requestScopes")
+                //throwing the resolve function out there for the returnScopes handler to deal with
+                //is such a war crime but im too lazy to figure out a less stupid way to do it
+                scopesResolve = resolve
+            })
+
+            //error if user has not authorized codeclient to write code
+            if (!scopes.includes("write_code")) {
+                sendEvent("showErrorMessage","Terracotta is missing permsissions, please run /auth in Minecraft")
+                process.exit(126)
+            }
+
+            //placeholder code
+            sendEvent("codeclient",'place swap') 
+            sendEvent("codeclient",'place H4sIAAAAAAACA+VWXWvCMBT9KyUwmMOHbX5s9GljDtmDLyqyD6TE9lrL0qQ0qZuI/303rUrUulWHg7KnNsm5556eHMqdkxET7rsk9tucBB6xszWpLp82GSfcxSWNfQQhRkGo0cNFlXhUUUTQD5AiBLKo7qGQoJwpjXNYsrd0RxdmoIx2TjhFUpvcOU7/wek+tp0rPJSuiPQuCzh2RBGSCUXsy7T5j1wjypg0WBIu6RQ8g+hqh4gn4S7RpVFyXaykZpTUFto/6qpAcDwagKtEfHoDrw8x0MeWzGBTs7SuFcSoVuuuEoVyAItJC8Y0YepbH0fMUdQ3CXFlkwcRRoIDV3gioqUhz/kfv/KrDSqzTBebN9Hc9HULd2p7a7/OZ6Gs/0dr62VJ7ksp7W2UIbnltLZZluS+ltLemzIkt5zW3h5ibe7IcxZSNTk/wz7nxhBSudjaqVUqh09gudz1He7GBnfBUS2Xu7nDfbPBfeBMFzE6g9hZ4v/0rvZE954FPg8xulZHeGDmtwt+wlIBe9WvPrsH3OuAlNQ3NWCSC4row6eyBpQlYHUg9gOuIWsh955nyYi6oIf4Y7U0imp54hOIA2X11IylHdc6+nGiDTpWQX0zKiYq7wc/XHwBFjf2p6cNAAA=') 
+            sendEvent("codeclient",'place go') 
+        }
+    },
+    "codeclientMessage": function(request) {
+        if (request.arguments == "place done") {
+            process.exit(0)
+        }
+    },
+    "returnScopes": function(request) {
+        if (scopesResolve) {
+            scopesResolve(request.arguments)
+            scopesResolve = undefined
+        }
+    }
+}
+
+process.stdin.on("data",data => {
+    let lines = data.toString().split("\r\n")
+    let json = JSON.parse(lines[2])
+    if (json.type == "request") {
+        if (requestHandlers[json.command]) {
+            requestHandlers[json.command](json)
+        }   
+    }
+})
--- src/extension.ts
+++ src/extension.ts
@@ -2,19 +2,17 @@ import * as cp from "child_process";
 import * as path from "path"
 import * as vscode from 'vscode';
 import {workspace} from "vscode"
-
-
-import {
-	LanguageClient,
-	LanguageClientOptions,
-	ServerOptions,
-} from 'vscode-languageclient/node';
+import { RawData, WebSocket } from 'ws';
+import { LanguageClient, LanguageClientOptions, ServerOptions } from 'vscode-languageclient/node';
 
 
 const config: vscode.WorkspaceConfiguration = workspace.getConfiguration("terracotta")
+const debuggers: {[key: string]: vscode.DebugSession} = {}
+
 let client: LanguageClient
 
-//file paths
+//==========[ file paths ]=========\
+
 let splitPath = __dirname.split("/")
 splitPath.pop()
 let bunPath = (splitPath.join("/")+"/node_modules/.bin/bun").replace(/ /g,"\\ ").replace(/"/g,'\\"')
@@ -24,10 +22,85 @@ if (!terracottaPath.endsWith("/")) { terracottaPath += "/" }
 
 let mainScriptPath = terracottaPath + "src/main.ts"
 
+//==========[ codeclient ]=========\
+
+let codeClientWS: WebSocket
+
+async function getCodeClientScopes(): Promise<string[]> {
+	return await new Promise<string[]>(resolve => {
+		codeClientWS.send("scopes")
+
+		//i have no idea what happens if a message other than the scopes return gets recieved
+		//i assume the code will just see "oh no i dont have the scopes" and request new ones
+		codeClientWS.once("message",message => {
+			let scopes = message.toString().split(" ")
+			resolve(scopes)
+		})
+	})
+}
+
+async function setupCodeClient() {
+	//client
+	codeClientWS = new WebSocket("ws://localhost:31375")
+    
+	codeClientWS.on("open",async () => {
+		//request write code permission if this doesnt already have it
+		let currentScopes = await getCodeClientScopes()
+
+		if (!currentScopes.includes("write_code")) {
+			codeClientWS.send("scopes write_code")
+		}
+	})
+
+	codeClientWS.on("message",(message: RawData | string) => {
+		message = message.toString()
+
+		for (const session of Object.values(debuggers)) {
+			session.customRequest("codeclientMessage",message)
+		}
+	})
+}
+
+//==========[ extension events ]=========\
 
 export function activate(context: vscode.ExtensionContext) {
-	let server: cp.ChildProcess
 	let outputChannel = vscode.window.createOutputChannel("Terracotta LSP")
+		
+	setupCodeClient()
+
+	//= set up debugger =\\
+
+	//split up all the async callbacks into their own group to avoid
+	//async'ing all the syncronous ones
+	vscode.debug.onDidReceiveDebugSessionCustomEvent(async event => {
+		//i would use an ACTUAL REQUEST for this but theres not a callback for that 
+		if (event.event == "requestScopes") {
+			event.session.customRequest("returnScopes",await getCodeClientScopes())
+		}
+	})
+
+	vscode.debug.onDidReceiveDebugSessionCustomEvent(event => {
+		if (event.event == "log") {
+			console.log(event.body)
+		}
+		else if (event.event == "showErrorMessage") {
+			vscode.window.showErrorMessage(event.body)
+		}
+		else if (event.event == "codeclient") {
+			codeClientWS.send(event.body)
+		}
+	})
+
+	vscode.debug.onDidStartDebugSession(session => {
+		debuggers[session.id] = session
+	})
+
+	vscode.debug.onDidTerminateDebugSession(session => {
+		delete debuggers[session.id]
+	})
+
+	//= set up language server =\\
+	let server: cp.ChildProcess
 
 	//lmao i am so sorry
 	let serverOptions: ServerOptions = async function() {
